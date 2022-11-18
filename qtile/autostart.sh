1ec802661d1ba89d2783f760943dc8346e073e51
arutonee <arutonee@proton.me>
2022-11-18 16:04:05 -0500

    Added Fischl theme.

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -2,10 +2,10 @@ killall picom xgifwallpaper
 xset -dpms
 xset s noblank
 xset s off
-sleep 0.5
-picom --corner-radius $1 &
-
 if [[ $# -gt 0 ]]; then
   sleep 0.5
-  xgifwallpaper "$HOME/.config/qtile/themes/$2/background.gif" -d $((100 / $(cat "$HOME/.config/qtile/themes/$2/fps"))) & #-s FILL &
+  xgifwallpaper "$HOME/.config/qtile/themes/$2/background.gif" -d $((100 / $(cat "$HOME/.config/qtile/themes/$2/fps"))) -s FILL &
 fi
+
+sleep 5
+picom --corner-radius $1 &
