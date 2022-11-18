f902a0194f9989a551fdee0934878fe7bf181794
arutonee <arutonee@proton.me>
2022-11-18 15:21:08 -0500

    Minor edits, removed Pastel theme.

--- qtile/mp42gif.sh
+++ qtile/mp42gif.sh
@@ -1,8 +1,10 @@
 if [[ $1 == "-h" ]]; then
   echo "mp42gif.sh [input] [fps] [width] [output] [every nth frame]"
 else
+  ffmpeg -i "$1" -vf palettegen /tmp/palette.png
   ffmpeg -i "$1" \
-    -vf "select=not(mod(n\,$5)),fps=$(($2 / $5)),scale=$3:-1:flags=lanczos" \
+    -i /tmp/palette.png \
+    -filter_complex "paletteuse=dither=none,select=not(mod(n\,$5)),fps=$(($2 / $5)),scale=$3:-1:flags=lanczos" \
     -vsync vfr \
     "$4"
 fi
