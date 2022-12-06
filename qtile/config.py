eaababc85a93a9d012715865ad326e69af6598b1
arutonee <arutonee@proton.me>
2022-12-06 16:08:06 -0500

    Added Hope theme to qtile.

--- qtile/config.py
+++ qtile/config.py
@@ -3,7 +3,7 @@ import toml
 import subprocess
 
 THEME_DIR = "~/.config/qtile/themes/"
-THEME_NAME = "astolfo"
+THEME_NAME = "hope"
 
 if not exists(expanduser(THEME_DIR + THEME_NAME + "/theme.toml")):
     THEME_NAME = "default"
@@ -37,31 +37,15 @@ keys = [
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    # Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
     Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
     Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
     Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
     Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    # Key(
-    #     [mod, "shift"],
-    #     "Return",
-    #     lazy.layout.toggle_split(),
-    #     desc="Toggle between split and unsplit sides of stack",
-    # ),
-    # Toggle between different layouts as defined below
     Key([mod], "Space", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
     Key([mod, "shift"], "r", lazy.restart(), desc="Restart Qtile"),
@@ -175,18 +159,18 @@ dgroups_app_rules = []  # type: list
 follow_mouse_focus = True
 bring_front_click = False
 cursor_warp = False
-floating_layout = layout.Floating(
-    float_rules=[
-        # # Run the utility of `xprop` to see the wm class and name of an X client.
-        # *layout.Floating.default_float_rules,
-        # Match(wm_class="confirmreset"),  # gitk
-        # Match(wm_class="makebranch"),  # gitk
-        # Match(wm_class="maketag"),  # gitk
-        # Match(wm_class="ssh-askpass"),  # ssh-askpass
-        # Match(title="branchdialog"),  # gitk
-        # Match(title="pinentry"),  # GPG key password entry
-    ]
-)
+# floating_layout = layout.Floating(
+#     float_rules=[
+#         # # Run the utility of `xprop` to see the wm class and name of an X client.
+#         # *layout.Floating.default_float_rules,
+#         # Match(wm_class="confirmreset"),  # gitk
+#         # Match(wm_class="makebranch"),  # gitk
+#         # Match(wm_class="maketag"),  # gitk
+#         # Match(wm_class="ssh-askpass"),  # ssh-askpass
+#         # Match(title="branchdialog"),  # gitk
+#         # Match(title="pinentry"),  # GPG key password entry
+#     ]
+# )
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
